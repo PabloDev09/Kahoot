707c5be93cd6c24e3906ecf542d2bf8e23d86064
PabloDev09 <pmg11@gmail.com>
2024-11-21 12:56:38 +0100

    V4.4.4 - Kahoot

--- app/src/main/java/es/iesjandula/kahoot/screens/MainActivity.kt
+++ app/src/main/java/es/iesjandula/kahoot/screens/MainActivity.kt
@@ -1,25 +1,20 @@
 package es.iesjandula.kahoot.screens
 
 import android.animation.ObjectAnimator
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import es.iesjandula.kahoot.R
 import es.iesjandula.kahoot.database.PreguntaDb
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: PreguntaDb
@@ -41,43 +36,34 @@ class MainActivity : AppCompatActivity() {
         }
 
         val btnJugar = findViewById<Button>(R.id.btnJugar)
-        val animatorUno = ObjectAnimator.ofFloat(findViewById<TextView>(R.id.tvBienvenido), "translationX", -50f, 0f)
-        animatorUno.duration = 1000
-        animatorUno.repeatCount = ObjectAnimator.INFINITE
-        animatorUno.repeatMode = ObjectAnimator.REVERSE
-        animatorUno.start()
+        val animator = ObjectAnimator.ofFloat(findViewById<TextView>(R.id.tvKahoot), "translationY", -100f, +100f)
+        animator.duration = 1800
+        animator.repeatCount = ObjectAnimator.INFINITE
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
 
-        val animatorDos = ObjectAnimator.ofFloat(findViewById<TextView>(R.id.tvKahoot), "translationX", 0f, +50f)
-        animatorDos.duration = 1000
-        animatorDos.repeatCount = ObjectAnimator.INFINITE
-        animatorDos.repeatMode = ObjectAnimator.REVERSE
-        animatorDos.start()
 
-        val animatorTres = ObjectAnimator.ofFloat(findViewById<CardView>(R.id.cardContainerButtons), "translationY", -100f, 100f)
-        animatorTres.duration = 1800
-        animatorTres.repeatCount = 1
-        animatorTres.repeatMode = ObjectAnimator.REVERSE
-        animatorTres.start()
+        btnJugar.setOnClickListener{
+            database.preguntaDao().getAllLive().observe(this) { listado ->
 
-        btnJugar.setOnClickListener{ startActivity(Intent(this, JugarActivity::class.java))}
-//        database.preguntaDao().getAllLive().observe(this) { listado ->
-//
-//            btnJugar.setOnClickListener {
-//                val numeroDePreguntas = listado.size
-//
-//                if (numeroDePreguntas < 8) {
-//                    val mensaje = if (numeroDePreguntas == 1) {
-//                        "Hay $numeroDePreguntas pregunta, es necesario 8 como mínimo"
-//                    } else {
-//                        "Hay $numeroDePreguntas preguntas, es necesario 8 como mínimo"
-//                    }
-//                    Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
-//                } else {
-//                    val intent = Intent(this, JugarActivity::class.java)
-//                    startActivity(intent)
-//                }
-//            }
-//        }
+                btnJugar.setOnClickListener {
+                    val numeroDePreguntas = listado.size
+
+                    if (numeroDePreguntas < 8) {
+                        val mensaje = if (numeroDePreguntas == 1) {
+                            "Hay $numeroDePreguntas pregunta, es necesario 8 como mínimo"
+                        } else {
+                            "Hay $numeroDePreguntas preguntas, es necesario 8 como mínimo"
+                        }
+                        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+                    } else {
+                        val intent = Intent(this, JugarActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
+
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
